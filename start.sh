34c4c189b0c9d168cca59792d18170b211ebdaf9
batmanhyd <155296058+batmanhyd@users.noreply.github.com>
2024-01-01 12:06:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mrcnu/MartinBot.git /MartinBot
+  git clone https://github.com/batmanhyd/MartinBot.git /MartinBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MartinBot
